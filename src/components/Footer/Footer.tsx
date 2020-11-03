04a88d79b1174b4a45db3c0a906c002e54b76b4c
Andrew Vyatkin <5979694+spelot@users.noreply.github.com>
2020-11-03 22:25:20 +0300

    fix styles for Header and Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,19 @@ import cn from 'classnames';
 import s from './Footer.module.scss';
 
 const Footer: FC = () => {
-  return <div className={cn(s.Footer)}>FOOTER</div>;
+  return (
+    <div className={cn(s.Footer)}>
+      <div className={cn(s.label)}>
+        Make with{' '}
+        <span role="img" aria-label="love" aria-labelledby="">
+          ❤️
+        </span>
+      </div>
+      <a className={cn(s.link)} href="https://zarmarathon.com/">
+        Ours Team
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
